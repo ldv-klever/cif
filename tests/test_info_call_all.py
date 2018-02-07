48704cc4262de64daea100c5dba12571971cd372
Ilya Shchepetkov <shchepetkov@ispras.ru>
2018-02-07 19:43:41 +0300

    Rename 4 tests

--- tests/test_info_call_all.py
+++ tests/test_info_call_all.py
@@ -5,19 +5,19 @@ import utils
 class TestInfoCallAll(utils.CIFTestCase):
     def test_simple_func_context_path(self):
         self.cif.run(cif_input='input/simple_call.c', aspect='aspect/info_call_all_func_context_path.aspect', stage='instrumentation')
-        self.compare(output='work/info.txt', expected='output/test_simple_func_context_path.txt')
+        self.compare(output='work/info.txt', expected='output/simple_call_info_call_all_func_context_path.txt')
 
     def test_simple_func_context_name(self):
         self.cif.run(cif_input='input/simple_call.c', aspect='aspect/info_call_all_func_context_name.aspect', stage='instrumentation')
-        self.compare(output='work/info.txt', expected='output/test_simple_func_context_name.txt')
+        self.compare(output='work/info.txt', expected='output/simple_call_info_call_all_func_context_name.txt')
 
     def test_simple_func_name(self):
         self.cif.run(cif_input='input/simple_call.c', aspect='aspect/info_call_all_func_name.aspect', stage='instrumentation')
-        self.compare(output='work/info.txt', expected='output/test_simple_func_name.txt')
+        self.compare(output='work/info.txt', expected='output/simple_call_info_call_all_func_name.txt')
 
     def test_simple_call_line(self):
         self.cif.run(cif_input='input/simple_call.c', aspect='aspect/info_call_all_call_line.aspect', stage='instrumentation')
-        self.compare(output='work/info.txt', expected='output/test_simple_call_line.txt')
+        self.compare(output='work/info.txt', expected='output/simple_call_info_call_all_call_line.txt')
 
 
 if __name__ == '__main__':
