c2e87b3c2717daec512b139eaa56ab68c8fab41b
Evgeny Novikov <novikov@ispras.ru>
2018-07-27 15:17:52 +0300

    Update Aspectator and fix appropriate test case

--- tests/test_info_define.py
+++ tests/test_info_define.py
@@ -5,6 +5,13 @@ import utils
 class TestInfoDefine(utils.CIFTestCase):
     def test_simple_info_define_all(self):
         self.cif.run(cif_input='input/simple.c', aspect='aspect/info_define_all.aspect', stage='instrumentation')
+        with open('work/info.txt') as fp:
+            lines = fp.readlines()
+        with open('work/info.txt', 'w') as fp:
+            # Skip OS specific defines.
+            for line in lines:
+                if not line.startswith('/usr/include/stdc-predef.h'):
+                    fp.write(line)
         self.compare(output='work/info.txt', expected='output/simple_call_info_define_all.txt')
 
 
