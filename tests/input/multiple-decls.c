a9e2b1fa137e8259b96b86b4d9e9df9614b405ca
Evgeny Novikov <novikov@ispras.ru>
2019-02-24 14:03:01 +0300

    Update Aspectator and add test case

---
+++ tests/input/multiple-decls.c
@@ -0,0 +1,6 @@
+typedef int T1;
+typedef int T1;
+typedef int *T2;
+typedef int *T2;
+T1 a;
+T2 b;
--- tests/test_info_introduce.py
+++ tests/test_info_introduce.py
@@ -7,6 +7,9 @@ class TestInfoIntroduce(utils.CIFTestCase):
         self.cif.run(cif_input='input/simple.c', aspect='aspect/info_introduce_all.aspect', stage='instrumentation')
         self.compare(output='work/info.txt', expected='output/simple_call_info_introduce_all.txt')
 
+    def test_info_introduce_multiple_decls(self):
+        self.cif.run(cif_input='input/multiple-decls.c', aspect='aspect/info_introduce_all.aspect', stage='instrumentation')
+        self.compare(output='work/info.txt', expected='output/info_introduce_multiple_decls.txt')
 
 if __name__ == '__main__':
     unittest.main()
