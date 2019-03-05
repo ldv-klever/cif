c8158daa94d0f47349574e3211579c4b379446ce
Evgeny Novikov <novikov@ispras.ru>
2019-03-05 08:14:29 +0300

    Remove absolute paths from CIF output
    Otherwise tests fail on another machine.

--- tests/test_complex-aspect-pattern-parameters.py
+++ tests/test_complex-aspect-pattern-parameters.py
@@ -11,7 +11,9 @@ class TestComplexAspectPatternParameters(utils.CIFTestCase):
     def test_strings_with_aspect_pattern_string_parameters(self):
         os.environ['PREFIX'] = 'pre'
         self.cif.run(cif_input='input/simple.c', aspect='aspect/strings-with-aspect-pattern-string-parameters.aspect', stage='instrumentation')
+        self.make_relpath('work/pre-pre-SOMETHING.txt')
         self.compare(output='work/pre-pre-SOMETHING.txt', expected='output/strings-with-aspect-pattern-string-parameters1.txt')
+        self.make_relpath('work/pre-pre-CONST.txt')
         self.compare(output='work/pre-pre-CONST.txt', expected='output/strings-with-aspect-pattern-string-parameters2.txt')
 
 if __name__ == '__main__':
