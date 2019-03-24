3e7483532e5b1813126f1604c3383ca5b3847c30
Evgeny Novikov <novikov@ispras.ru>
2019-03-24 11:32:59 +0300

    Improve test cases added recently

--- tests/output/c-backend/short-wide-char-str.c
+++ tests/output/c-backend/short-wide-char-str.c
@@ -4,7 +4,8 @@
 #define LDV_LROTATE(X,Y) (((X) << (Y)) | ((X) >> (__CHAR_BIT__ * sizeof (X) - Y)))
 #define LDV_RROTATE(X,Y) (((X) >> (Y)) | ((X) << (__CHAR_BIT__ * sizeof (X) - Y)))
 typedef struct __va_list_tag __va_list_tag;
-#line 2 "input/c-backend/wide-char-str.c"
+#line 1 "input/c-backend/wide-char-str.c"
+short unsigned int var1[14U] = L"Hello, world!";
 short unsigned int var2[17U] = L"Здравствуй,\tмир!";
-#line 4 "input/c-backend/wide-char-str.c"
+char const var3[14U] = "Hello, world!";
 char const var4[31U] = "Здравствуй,\b\nмир!";
