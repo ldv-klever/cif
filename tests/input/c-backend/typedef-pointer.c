a4926189ae2c868de65082467f1efc65c963c6af
Evgeny Novikov <novikov@ispras.ru>
2019-05-27 18:17:25 +0300

    Update Aspectator and update corresponding test case

--- tests/input/c-backend/typedef-pointer.c
+++ tests/input/c-backend/typedef-pointer.c
@@ -1,3 +1,14 @@
 typedef int T1;
 typedef int *T2;
+typedef T2 T3;
+typedef T2 *T4;
+T1 var1;
+T2 var2;
+T3 var3;
+T4 var4;
+T1 *var5;
+T2 *var6;
+T3 *var7;
+T4 *var8;
 void func(T1, T2, T1 *, T2 *, T1 **, T2 **);
+void gunc(T1 arg1, T2 arg2, T1 *arg3, T2 *arg4, T1 **arg5, T2 **arg6) {}
--- tests/output/c-backend/typedef-pointer.c
+++ tests/output/c-backend/typedef-pointer.c
@@ -7,4 +7,17 @@ typedef struct __va_list_tag __va_list_tag;
 #line 1 "input/c-backend/typedef-pointer.c"
 typedef int T1;
 typedef int *T2;
+typedef T2 T3;
+typedef T2 *T4;
+T1 var1;
+T2 var2;
+T3 var3;
+T4 var4;
+T1 *var5;
+T2 *var6;
+T3 *var7;
+T4 *var8;
 void func(T1, T2, T1 *, T2 *, T1 **, T2 **);
+void gunc(T1 arg1, T2 arg2, T1 *arg3, T2 *arg4, T1 **arg5, T2 **arg6)
+{
+}
