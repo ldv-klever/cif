a9ed1cb237c993a815dba55c598ae5d7d51db503
Ilya Shchepetkov <shchepetkov@ispras.ru>
2021-08-16 14:25:11 +0300

    Update Aspectator and add new test

---
+++ tests/test_pointcuts.py
@@ -0,0 +1,7 @@
+import utils
+
+
+class TestPointcuts(utils.CIFTestCase):
+    def test_same_name(self):
+        self.cif.run(cif_input='input/simple.c', aspect='aspect/pointcuts_with_same_name.aspect', stage='instrumentation')
+        self.assertEqual(self.cif.status, 1)
