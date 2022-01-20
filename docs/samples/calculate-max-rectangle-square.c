5a5d0613214cc36729ced0290aae218dbc5fc73a
Evgeny Novikov <novikov@ispras.ru>
2022-01-20 18:44:39 +0300

    Add samples for upcoming tutorial

---
+++ docs/samples/calculate-max-rectangle-square.c
@@ -0,0 +1,43 @@
+/* This is a very simple program that finds out a rectangle with a maximum square from a provided list of rectangle
+   heights and widths. It is intended only for demonstration of CIF capabilities. Please, do not use it anywhere since
+   it contains several issues. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX(a, b) (a > b ? a : b)
+
+struct rectangle
+{
+    unsigned int height;
+    unsigned int width;
+    unsigned int square;
+};
+
+unsigned int calculate_rectangle_square(struct rectangle *r)
+{
+    r->square = r->height * r->width;
+    return r->square;
+}
+
+int main(int argc, const char *argv[])
+{
+    unsigned int rectangles_num;
+    struct rectangle *rectangles;
+    unsigned int cur_max_rectangle_square = 0;
+
+    rectangles_num = (unsigned int)(argc / 2);
+    rectangles = calloc(rectangles_num, sizeof(*rectangles));
+
+    for (int i = 0; i < rectangles_num; i++) {
+        struct rectangle *cur_rectangle = rectangles + i;
+        cur_rectangle->height = atoi(argv[2 * i + 1]);
+        cur_rectangle->width = atoi(argv[2 * i + 2]);
+        calculate_rectangle_square(cur_rectangle);
+        cur_max_rectangle_square = MAX(cur_max_rectangle_square, cur_rectangle->square);
+    }
+
+    printf("Maximum rectangle square is %u\n", cur_max_rectangle_square);
+
+    return 0;
+}
