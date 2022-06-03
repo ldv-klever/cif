ae73aa22bf172258b3784dcc0b9047b59227ff17
Evgeny Novikov <novikov@ispras.ru>
2022-06-03 22:02:53 +0300

    Update Aspectator supporting $var_init_list_json and add test for it

--- tests/test_query_init_global.py
+++ tests/test_query_init_global.py
@@ -18,3 +18,7 @@ class TestQueryInitGlobal(utils.CIFTestCase):
     def test_large_array_query_init_list(self):
         self.cif.run(cif_input='input/large_array.c', aspect='aspect/query_init_list.aspect', stage='instrumentation')
         self.compare(output='work/info.txt', expected='output/large_array_call_query_init_list.txt')
+
+    def test_complex_struct_query_init_list_json(self):
+        self.cif.run(cif_input='input/complex_struct.c', aspect='aspect/query_init_list_json.aspect', stage='instrumentation')
+        self.compare(output='work/info.txt', expected='output/complex_struct_call_query_init_list_json.txt')
